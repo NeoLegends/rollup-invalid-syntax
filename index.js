e3225a9d1e65282e6ac53477c48f2acde5f8aa79
Moritz Gunz <moritz.gunz@gmail.com>
2018-05-06 21:30:19 +0200

    :memo: Add note where file comes from

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// taken from https://github.com/calvinmetcalf/rollup-plugin-node-globals/blob/master/src/global.js
+
 export default typeof global !== "undefined" ? global :
             typeof self !== "undefined" ? self :
             typeof window !== "undefined" ? window : {}
